0bc787f5cdbea9dece339399b1b7b62bf7904cce
vizualz <pm@boardhunt.com>
2014-10-05 23:48:11 -0700

    Test the designer file

--- m/surfboard.aspx.designer.cs
+++ m/surfboard.aspx.designer.cs
@@ -173,14 +173,7 @@ namespace BoardHunt.m {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblPhoneData;
-        
-        /// <summary>
-        /// pnlGearItem control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
+
         protected global::System.Web.UI.WebControls.Panel pnlGearItem;
         
         /// <summary>
